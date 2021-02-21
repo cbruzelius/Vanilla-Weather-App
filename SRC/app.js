6ac84182fa027d3644d6d81e37dd7c397704f3c0
cbruzelius <76457870+cbruzelius@users.noreply.github.com>
2021-02-20 16:02:02 -0800

    Added weather elements and main weather icon

--- SRC/app.js
+++ SRC/app.js
@@ -29,16 +29,43 @@ let localTime = formatAMPM(now);
 let currentDT=document.querySelector("#current-DT");
 currentDT.innerHTML= `${day} ${localTime}`;
 
+
+//Display Temperature//
+function displayTemperature(response){
+  console.log(response.data);
+  let defaultTemp= Math.round(response.data.main.temp);
+  let defaultCity= response.data.name
+  let defaultDescription= response.data.weather[0].description;
+  let defaultHumidity= response.data.main.humidity;
+  let defaultWind= Math.round(response.data.wind.speed);
+  let defaultIcon=response.data.weather[0].icon;
+ 
+ 
+  let temperatureElement=document.querySelector("#temperature");
+  let cityElement=document.querySelector("#city");
+  let descriptionElement=document.querySelector("#description");
+  let humidityElement=document.querySelector("#humidity");
+  let windElement=document.querySelector("#wind");
+  let iconElement=document.querySelector("#weather-icon");
+  
+  temperatureElement.innerHTML=`${defaultTemp}`;
+  cityElement.innerHTML=`${defaultCity}`;
+  descriptionElement.innerHTML=`${defaultDescription}`;
+  humidityElement.innerHTML=`${defaultHumidity}`;
+  windElement.innerHTML=`${defaultWind}`;
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${defaultIcon}@2x.png`);
+  iconElement.setAttribute("alt", `${defaultIcon}`);
+
+}
+
+
 let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
 let apiKey = "7810de17cb3e4ad4b94e69a98fbe80a0";
+let city = "Sunnyvale"
 let units = "metric";
-let apiURL = `${apiEndpoint}q=Sunnyvale&units=${units}&appid=${apiKey}`;
+let apiURL = `${apiEndpoint}q=${city}&units=${units}&appid=${apiKey}`;
 
-axios.get(apiURL).then(showDefaultTemp);
+axios.get(apiURL).then(displayTemperature);
 
-function showDefaultTemp(response){
-  let defaultTemp= Math.round(response.data.main.temp);
-  let temperature=document.querySelector("#temperature");
-  temperature.innerHTML=`${defaultTemp}`;
-}
+
 
