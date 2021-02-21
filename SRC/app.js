6080aad279284bcfd9fcff78c443cc264ce6486e
cbruzelius <76457870+cbruzelius@users.noreply.github.com>
2021-02-21 01:06:47 -0800

    Added Current City Button, Search City form

--- SRC/app.js
+++ SRC/app.js
@@ -66,6 +66,7 @@ function displayTemperature(response){
   
   axios.get(apiURL).then(displayTemperature);
 
+  //Search City//
 function search (city){
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
   let apiKey = "7810de17cb3e4ad4b94e69a98fbe80a0";
@@ -80,7 +81,53 @@ function recievesCity(event){
   let cityInput = document.querySelector("#search-city");
   search(cityInput.value);
 }
-
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", recievesCity)
+
+//Current City//
+function getCurrentWeather(event){
+  event.preventDefault();
+
+  function displayPosition(response){
+  let localTemp= Math.round(response.data.main.temp);
+  let localCity=response.data.name;
+  let localDescription= response.data.weather[0].description;
+  let localHumidity= response.data.main.humidity;
+  let localWind= Math.round(response.data.wind.speed);
+  let localIcon=response.data.weather[0].icon;
+ 
+ 
+  let temperatureElement=document.querySelector("#temperature");
+  let cityElement=document.querySelector("#city");
+  let descriptionElement=document.querySelector("#description");
+  let humidityElement=document.querySelector("#humidity");
+  let windElement=document.querySelector("#wind");
+  let iconElement=document.querySelector("#weather-icon");
+  
+  temperatureElement.innerHTML=`${localTemp}`;
+  cityElement.innerHTML=`${localCity}`;
+  descriptionElement.innerHTML=`${localDescription}`;
+  humidityElement.innerHTML=`${localHumidity}`;
+  windElement.innerHTML=`${localWind}`;
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${localIcon}@2x.png`);
+  iconElement.setAttribute("alt", `${localIcon}`);
+}
+
+function showPosition(position){
+    let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
+    let apiKey = "7810de17cb3e4ad4b94e69a98fbe80a0";
+    let units = "metric";
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    let localApiURL = `${apiEndpoint}lat=${latitude}&lon=${longitude}&units=${units}&appid=${apiKey}`;
+  
+    axios.get(localApiURL).then(displayPosition);
+}
+
+navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+
+let currentLocalWeather = document.querySelector("#current-city");
+currentLocalWeather.addEventListener("click", getCurrentWeather);
+
